7a7bf260022ffc43469d6d6ee9dbdcdbb7fc5327
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2022-03-21 16:51:48 +0530

    added CBSE Project

--- routes/rootRoute.js
+++ routes/rootRoute.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
+
+// <==== CBSE PROJECT REMAS STRT ====>
+const CBSEProject = `https://drive.google.com/drive/folders/1VpvgyX_oP0e0wry5I0UZTBnY6Ir7Sd7M?usp=sharing`
+// /cbse
+router.get('/cbse', async (req, res) => {
+    res.redirect(CBSEProject)
+})
+// /remas
+router.get('/cbse', async (req, res) => {
+    res.redirect(CBSEProject)
+})
+// <==== CBSE PROJECT REMAS ENDS ====>
+
+
 // /arc-sw.js
 router.get('/arc-sw.js', async (req, res) => {
     res.sendFile(`${process.cwd()}/views/arc-sw.js`)
